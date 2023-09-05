0ffe11e3da5e86f5cf83620f6e4cd469ee729fd2
Shroomy Shroom <sporeventexplosion@gmail.com>
2023-09-05 10:35:33 +0000

    cleanup

--- sdl2.c
+++ sdl2.c
@@ -13,7 +13,6 @@ struct metronome_state {
     float *clip_h;
     // number of samples after the beat of the current position, may be non-integer
     double beat_offset;
-    // uint64_t rand_state;
     double period;
     int nbeats;
     int beat;
@@ -24,34 +23,11 @@ const size_t CLIP_LEN = 4800;
 const double TWO_PI = M_PI * 2.0;
 const int SAMPLE_RATE = 48000;
 
-// "angular velocity" of the tone in radians/sample
-// const double TONE_ANGLE = 440.0 / SAMPLE_RATE * TWO_PI;
-// frequency in cycles/sample
-
-/*
-uint64_t xorshift64star(uint64_t *x) {
-    *x ^= *x >> 12;
-    *x ^= *x << 25;
-    *x ^= *x >> 27;
-    return *x * 0x2545f4914f6cdd1dul;
-}
-*/
-
-// const double TWO53 = (double) (1ul << 53ul);
-
-/*
-double rand_double(uint64_t *state) {
-    uint64_t n = xorshift64star(state) & ((1ul << 53ul) - 1ul);
-    return ((double) n) / TWO53;
-}
-*/
-
 void audio_callback(void* userdata, uint8_t *bytes, int bytelen) {
     struct metronome_state *state = (struct metronome_state *)userdata;
     int count = bytelen / 4;
     float *samples = (float *)bytes;
     double period = state->period;
-    // uint64_t *rand_state = &state->rand_state;
     int nbeats = state->nbeats;
     int beat = state->beat;
 
@@ -65,16 +41,6 @@ void audio_callback(void* userdata, uint8_t *bytes, int bytelen) {
         if (beat_offset < CLIP_LEN) {
             size_t index = ((uint32_t) beat_offset) % CLIP_LEN;
             sample = clip[index];
-            // sample = (float) (sin(TONE_ANGLE * beat_offset));
-            // double t = TONE_FREQ * beat_offset;
-            // double t = TONE_FREQ * beat_offset + 0.5;
-            // t = t - floor(t);
-            // sample = t < 0.5 ? 1.0f : -1.0f;
-            // sample = t * 2.0 - 1.0;
-            // sample = fabs(t - 0.5) * 2.0;
-            // sample = rand_double(rand_state) * 2.0 - 1.0;
-            // sample = copysignf(powf(fabsf(sample), 0.1f), sample);
-            // printf("%f\n", sample);
         }
         samples[i] = sample;
 
@@ -151,7 +117,6 @@ int main(int argc, char **argv) {
     state->period = period;
     state->nbeats = nbeats;
     state->beat = 0;
-    // state->rand_state = 1ul;
     want.userdata = state;
 
     dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
